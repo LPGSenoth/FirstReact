21cbb57fa7e5f5968ba3ff079c5707366de2c2fd
LPGSenoth <163125498+LPGSenoth@users.noreply.github.com>
2024-03-26 21:58:11 +0100

    découverte

--- FirstReact/src/App.jsx
+++ FirstReact/src/App.jsx
@@ -1,5 +1,30 @@
+const todos = [
+  'Chier sur mes potes',
+  'Faire en sorte que le caca fasse pipi',
+  'Bref encore et toujours le caca'
+]
+
 function App() {
-  return "Bonjour les gens";
+  return <>
+    <Title color="grey" id="monId" className="demo">
+      My component
+    </Title>
+    <input type="text" />
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto sunt facere architecto doloremque qui accusantium vel, praesentium harum perferendis sed eius consequatur rem, cumque delectus. Debitis corporis ipsum accusantium? Libero.
+    </p>
+    <ul>
+      {todos.map(todo =>(<li key={todo}>{todo}</li>))}
+    </ul>
+  </>
+}
+
+function Title({color, hidden, ...props}){
+  if(hidden) {
+    return null
+  }
+
+  return <h1 style={{color: color}}{...props}></h1>
 }
 
 export default App
